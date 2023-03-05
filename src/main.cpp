d99e44d289de8db44c63536c11b4d15bdc2579e4
Nomand <shpagin@imail.coresar.ru>
2023-03-05 18:42:49 -0300

    add vertex and fragment shaders

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,36 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+GLfloat point[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+//Вертексный шейдер
+const char* vertex_shader =
+        "#version 460\n"
+        "layout(location = 0) in vec3 vertex_position;"
+        "layout(location = 1)in vec3 vertex_color;"
+        "out vec3 color;"
+        "void main(){"
+        "   color = vertex_color;"
+        "   gl_Position = vec4(vertex_position, 1.0);"
+        "}";
+//Фрагментный шейдер
+const char* fragment_shader =
+        "#version 460\n"
+        "in vec3 color;"
+        "out vec4 frag_color;"
+        "void main(){"
+        "   frag_color = vec4(color, 1.0);"
+        "}";
+
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
@@ -31,7 +61,7 @@ int main(void)
 
     glfwWindowHint(GLFW_OPENGL_PROFILE ,GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
-    pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Hello World", NULL, NULL);
+    pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", NULL, NULL);
     if (!pWindow)
     {
         std::cout << "window not opened!" << std::endl;
@@ -53,7 +83,46 @@ int main(void)
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-    glClearColor(0, 1, 0, 1);
+    glClearColor(1, 1, 0, 1);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER); //создаем вертексный шейдер
+    glShaderSource(vs, 1, &vertex_shader, nullptr); //передаем программу в шейдер
+    glCompileShader(vs); //компилируем
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER); //создаем фрагментный шейдер
+    glShaderSource(fs, 1, &fragment_shader, nullptr); //передаем программу в шейдер
+    glCompileShader(fs); //компилируем
+
+    GLuint shader_program = glCreateProgram(); //создаем шейдер-программу
+    glAttachShader(shader_program, vs); //привязываем скомпилированные шейдеры
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program); //линкуем
+
+    glDeleteShader(vs); //отчищайем шейдеры
+    glDeleteShader(fs);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint vertex_arra_object = 0;
+    glGenVertexArrays(1, &vertex_arra_object);
+    glBindVertexArray(vertex_arra_object);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
@@ -61,6 +130,9 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader_program);
+        glBindVertexArray(vertex_arra_object);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
 
